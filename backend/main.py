36db238d7861f04154808047953872dc3a05d02f
Tej01sai <sai727868@gmail.com>
2025-06-18 12:50:57 +0530

    done relating the delpoyed frontend link

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,8 @@ logging.basicConfig(level=logging.INFO)
 # === CORS ===
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["http://localhost:3000",
+        "https://mental-wellness.vercel.app",], 
       # Update to your frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
