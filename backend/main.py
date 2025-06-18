5f3d816d1e39cf77379de8f364de326fe9bdb2f1
Tej01sai <sai727868@gmail.com>
2025-06-18 12:48:56 +0530

    backend issue solved

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,8 @@ logging.basicConfig(level=logging.INFO)
 # === CORS ===
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update to your frontend URL in production
+    allow_origins=["*"], 
+      # Update to your frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
