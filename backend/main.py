0ae722112cebf55929665ac4b72f4fe9a3ae03eb
Tej01sai <sai727868@gmail.com>
2025-06-18 13:08:35 +0530

    false credentials

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
       # Update to your frontend URL in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
